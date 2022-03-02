dbc5a4a4f72b2552035549126eff031a041dc395
esrakay <91302003+esrakay@users.noreply.github.com>
2022-03-02 23:55:22 +0100

    Controller for Job title added

--- hrms/src/main/java/com/hrms/api/controllers/JobTitleController.java
+++ hrms/src/main/java/com/hrms/api/controllers/JobTitleController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hrms.business.abstracts.JobTitleService;
 import com.hrms.core.DataResult;
+import com.hrms.core.Result;
 import com.hrms.entities.concretes.JobTitle;
 
 @RestController
@@ -26,5 +30,25 @@ public class JobTitleController {
 	@GetMapping("/getall")
 	public DataResult<List<JobTitle>> getAll(){
 		return this.jobTitleService.getAll(); 
+	}
+	
+	@GetMapping("/getByTitle")
+	public DataResult<List<JobTitle>> getByTitleStartsWith(@RequestParam String title){
+		return this.jobTitleService.getByTitleStartsWith(title);
+	}
+	
+	@GetMapping("getAllByPage")
+	public DataResult<List<JobTitle>> getAll(int pageNo, int pageSize){
+		return this.jobTitleService.getAll(pageNo, pageSize);
+	}
+	
+	@GetMapping("/getAllDesc")
+	public DataResult<List<JobTitle>> getAllSorted(){
+		return this.jobTitleService.getAllSorted();
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody JobTitle jobTitle) {
+		return this.jobTitleService.add(jobTitle);
 	}
 }
