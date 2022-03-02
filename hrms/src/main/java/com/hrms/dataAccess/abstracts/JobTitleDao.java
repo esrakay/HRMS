4f8a816a0404078dce258b4d1d7012f32bf13595
esrakay <91302003+esrakay@users.noreply.github.com>
2022-03-02 23:56:40 +0100

    new method added

--- hrms/src/main/java/com/hrms/dataAccess/abstracts/JobTitleDao.java
+++ hrms/src/main/java/com/hrms/dataAccess/abstracts/JobTitleDao.java
@@ -1,9 +1,11 @@
 package com.hrms.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hrms.entities.concretes.JobTitle;
 
 public interface JobTitleDao extends JpaRepository<JobTitle, Integer>{
-
+	List<JobTitle> getByTitleStartsWith(String title); 
 }
