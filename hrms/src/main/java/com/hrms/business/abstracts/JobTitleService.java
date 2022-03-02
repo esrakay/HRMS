2854f5c2d689a57351f261e95368fe545e81f18c
esrakay <91302003+esrakay@users.noreply.github.com>
2022-03-02 23:57:46 +0100

    new methods added

--- hrms/src/main/java/com/hrms/business/abstracts/JobTitleService.java
+++ hrms/src/main/java/com/hrms/business/abstracts/JobTitleService.java
@@ -3,8 +3,13 @@ package com.hrms.business.abstracts;
 import java.util.List;
 
 import com.hrms.core.DataResult;
+import com.hrms.core.Result;
 import com.hrms.entities.concretes.JobTitle;
 
 public interface JobTitleService {
 	public DataResult<List<JobTitle>> getAll(); 
+	public DataResult<List<JobTitle>> getAll(int pageNo, int pageSize);
+	public DataResult<List<JobTitle>> getAllSorted();
+	public DataResult<List<JobTitle>> getByTitleStartsWith(String title);
+	public Result add(JobTitle jobTitle);
 }
