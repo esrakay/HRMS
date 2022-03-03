625e9da74f332326128f84e37bffab9de770ab3d
esrakay <91302003+esrakay@users.noreply.github.com>
2022-03-03 22:16:31 +0100

    manager for candidate entity added

---
+++ hrms/src/main/java/com/hrms/business/concretes/CandidateManager.java
@@ -0,0 +1,45 @@
+package com.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.hrms.business.abstracts.CandidateService;
+import com.hrms.core.DataResult;
+import com.hrms.core.Result;
+import com.hrms.core.SuccessDataResult;
+import com.hrms.core.SuccessResult;
+import com.hrms.dataAccess.abstracts.CandidateDao;
+import com.hrms.entities.concretes.Candidate;
+
+@Service
+public class CandidateManager implements CandidateService{
+	
+	private CandidateDao candidateDao;
+	
+	@Autowired
+	public CandidateManager(CandidateDao candidateDao) {
+		this.candidateDao = candidateDao;
+	}
+
+	@Override
+	public Result add(Candidate candidate) {
+		this.candidateDao.save(candidate);
+		return new SuccessResult("Candidate added");
+	}
+
+	@Override
+	public DataResult<List<Candidate>> getAll() {
+		return new SuccessDataResult<List<Candidate>>(this.candidateDao.findAll());
+	}
+
+	@Override
+	public DataResult<List<Candidate>> getAll(int pageNo, int pageSize) {
+		Pageable pageable = PageRequest.of(pageNo-1, pageSize);
+		return new SuccessDataResult<List<Candidate>>(this.candidateDao.findAll(pageable).getContent());
+	}
+
+}
