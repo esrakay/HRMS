6558a217f78056e1251a9cd2de2530b5ab915edc
esrakay <91302003+esrakay@users.noreply.github.com>
2022-03-03 22:16:46 +0100

    controller for candidate entity added

---
+++ hrms/src/main/java/com/hrms/api/controllers/CandidateController.java
@@ -0,0 +1,42 @@
+package com.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hrms.business.abstracts.CandidateService;
+import com.hrms.core.DataResult;
+import com.hrms.core.Result;
+import com.hrms.entities.concretes.Candidate;
+
+@RestController
+@RequestMapping("/api/candidates")
+public class CandidateController {
+	
+	private CandidateService candidateService;
+	
+	@Autowired
+	public CandidateController(CandidateService candidateService) {
+		this.candidateService = candidateService;
+	}
+	
+	@GetMapping("/getall")
+	public DataResult<List<Candidate>> getAll(){
+		return this.candidateService.getAll();
+	}
+	
+	@GetMapping("/getAllByPage")
+	public DataResult<List<Candidate>> getAll(int pageNo, int pageSize){
+		return this.candidateService.getAll(pageNo, pageSize);
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody Candidate candidate) {
+		return this.candidateService.add(candidate); 
+	}
+}
