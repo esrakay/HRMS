598bf5fd7b0dbb048c2c0ef41459be8317e8ed73
esrakay <91302003+esrakay@users.noreply.github.com>
2022-03-04 01:56:20 +0100

    Controller for employer entity

---
+++ hrms/src/main/java/com/hrms/api/controllers/EmployerController.java
@@ -0,0 +1,60 @@
+package com.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hrms.business.abstracts.EmployerService;
+import com.hrms.core.DataResult;
+import com.hrms.core.Result;
+import com.hrms.entities.concretes.Employer;
+
+@RestController
+@RequestMapping("/api/employers")
+public class EmployerController {
+	
+	private EmployerService employerService;
+	
+	@Autowired
+	public EmployerController(EmployerService employerService) {
+		super();
+		this.employerService = employerService;
+	}
+	
+	
+	@GetMapping("/getall")
+	public DataResult<List<Employer>> getAll(){
+		return this.employerService.getAll();
+	}
+	
+	@GetMapping("/getAllDesc")
+	public DataResult<List<Employer>> getAllSorted(){
+		return this.employerService.getAllSorted();
+	}
+	
+	@GetMapping("/getAllByPage")
+	public DataResult<List<Employer>> getAll(int pageNo, int pageSize){
+		return this.employerService.getAll(pageNo, pageSize);
+	}
+	
+	@GetMapping("/getByCompanyName")
+	public DataResult<Employer> getByCompanyName(@RequestParam String companyName){
+		return this.employerService.getByCompanyName(companyName);
+	}
+	
+	@GetMapping("/getByCompanyNameStartsWith")
+	public DataResult<List<Employer>> getByCompanyNameStartsWith(@RequestParam String companyName){
+		return this.employerService.getByCompanyNameStartsWith(companyName);
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody Employer employer) {
+		return this.employerService.add(employer);
+	}
+}
