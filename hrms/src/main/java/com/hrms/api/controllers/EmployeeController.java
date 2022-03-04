a0ac5fffc87b7dc3b838a5589ef3946c736f2b80
esrakay <91302003+esrakay@users.noreply.github.com>
2022-03-04 02:46:46 +0100

    controller for employee entity added

---
+++ hrms/src/main/java/com/hrms/api/controllers/EmployeeController.java
@@ -0,0 +1,48 @@
+package com.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hrms.business.abstracts.EmployeeService;
+import com.hrms.core.DataResult;
+import com.hrms.core.Result;
+import com.hrms.entities.concretes.Employee;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+	
+	private EmployeeService employeeService;
+	
+	@Autowired
+	public EmployeeController (EmployeeService employeeService) {
+		this.employeeService = employeeService; 
+	}
+	
+	@GetMapping("/getall")
+	public DataResult<List<Employee>> getAll(){
+		return this.employeeService.getAll();
+	}
+	
+	@GetMapping("/getByFirstName")
+	public DataResult<List<Employee>> getByFirstName(@RequestParam String firstName){
+		return this.employeeService.getByFirstName(firstName);
+	}
+	
+	@GetMapping("/getByLastName")
+	public DataResult<List<Employee>> getByLastName(@RequestParam String lastName){
+		return this.employeeService.getByLastName(lastName);
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody Employee employee) {
+		return this.employeeService.add(employee);
+	}
+}
